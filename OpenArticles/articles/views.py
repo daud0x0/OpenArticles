eedff0b0a9c3affd49e3e9fd16e8e768e1be573a
Md Daud <daudmdnew2@gmail.com>
2022-09-24 17:00:42 +0530

    add some styles

--- OpenArticles/articles/views.py
+++ OpenArticles/articles/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome to OpenArticles!");
+    return render(request, "articles/index.html")
