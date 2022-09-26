bc222994391c013c28f7ada8edc49a04dd3fb1df
Md Daud <daudmdnew2@gmail.com>
2022-09-26 18:11:50 +0530

    add more content

--- OpenArticles/articles/views.py
+++ OpenArticles/articles/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "articles/index.html")
+
+
+def createPage(request):
+    return render(request, "articles/index.html"),
